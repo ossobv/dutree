dea311c9f8d09e78620ba9f8e44aea54ac8b61f4
Walter Doekes <walter+github@wjd.nu>
2017-12-21 10:27:54 +0100

    Initial commit with dutree.

---
+++ README.rst
@@ -0,0 +1,33 @@
+dutree :: Disk usage summary
+============================
+
+*dutree* shows a summary of the directories/files which take up the most space.
+
+Example usage::
+
+    $ dutree /srv
+
+Annotated output, where only paths of >4G are shown::
+
+     12.1 G  /srv/data/audiofiles/
+              ^-- audiofiles contains files/dirs with a total of 12.1G
+                  but it does NOT contain a single dir or file larger
+                  than 4G.
+      4.3 G  /srv/data/callrecordings/unmatched/
+      4.5 G  /srv/data/fax/
+     17.5 G  /srv/data/playlists/
+     34.4 G  /srv/data/twinfield_invoices/1/
+      7.8 G  /srv/data/*
+               ^-- data contains more files/directories than shown above
+                   but those that don't total above 4G are merged into
+                   this "leftover" node. That includes /twinfield_invoices/
+                   except for the /1/ subdirectory which already has its
+                   individual listing.
+     32   B  /srv/*
+               ^-- there was only /data/ in /srv/, but the directory itself
+                   also takes up a tiny bit of space
+      -----
+     80.6 G  TOTAL (86558511658)
+
+**NOTE**: The directories do not count the size of themselves, only of their
+contents. This explains any discrepancies with ``du -sb`` output.
---
+++ setup.py
@@ -0,0 +1,50 @@
+# dutree -- a quick and memory efficient disk usage scanner
+# Copyright (C) 2017  Walter Doekes, OSSO B.V.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from distutils.core import setup
+from os.path import dirname, join
+
+
+if __name__ == '__main__':
+    long_descriptions = []
+    with open(join(dirname(__file__), 'README.rst')) as file:
+        long_descriptions.append(file.read())
+    version = '0.1'
+
+    setup(
+        name='dutree',
+        version=version,
+        scripts=['dutree'],
+        description='quick and efficient disk usage scanner',
+        long_description=('\n\n\n'.join(long_descriptions)),
+        author='Walter Doekes, OSSO B.V.',
+        author_email='wjdoekes+dutree@osso.nl',
+        url='https://github.com/ossobv/dutree',
+        license='GPLv3+',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            ('License :: OSI Approved :: GNU General Public License v3 '
+             'or later (GPLv3+)'),
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Topic :: System :: Filesystems',
+            'Topic :: Utilities',
+        ],
+    )
+
+# vim: set ts=8 sw=4 sts=4 et ai tw=79:
